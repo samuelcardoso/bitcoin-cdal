cf645125bf179d571cae4a1bdb824ee33f475606
Gleisson de Assis <gleisson.assis@gmail.com>
2018-05-24 18:24:51 -0300

    Adding trusted transactions control...

--- src/workers/bosWorker.js
+++ src/workers/bosWorker.js
@@ -35,17 +35,21 @@ module.exports = function(dependencies) {
       logger.info('[BOSWorker] Total of blockchain transactions', r.transactions.length);
 
       for (var i = 0; i < r.transactions.length; i++) {
-        if (r.transactions[i].category === 'send' || r.transactions[i].category === 'receive' ) {
-          logger.info('[BOSWorker] Parsing the transaction', r.transactions[i].txid, JSON.stringify(r.transactions[i]));
+        if (r.transactions[i].trusted) {
+          if (r.transactions[i].category === 'send' || r.transactions[i].category === 'receive' ) {
+            logger.info('[BOSWorker] Parsing the transaction', r.transactions[i].txid, JSON.stringify(r.transactions[i]));
 
-          var pTransaction = new Promise(function(resolve) {
-            transactionBO.parseTransaction(r.transactions[i])
-              .then(resolve)
-              .catch(resolve);
-          });
-          p.push(pTransaction);
+            var pTransaction = new Promise(function(resolve) {
+              transactionBO.parseTransaction(r.transactions[i])
+                .then(resolve)
+                .catch(resolve);
+            });
+            p.push(pTransaction);
+          } else {
+            logger.info('[BOSWorker] Ignoring the transaction', r.transactions[i].txid, JSON.stringify(r.transactions[i]));
+          }
         } else {
-          logger.info('[BOSWorker] Ignoring the transaction', r.transactions[i].txid, JSON.stringify(r.transactions[i]));
+          logger.info('[BOSWorker] The transaction is not trusted', r.transactions[i].txid, JSON.stringify(r.transactions[i]));
         }
       }
 
